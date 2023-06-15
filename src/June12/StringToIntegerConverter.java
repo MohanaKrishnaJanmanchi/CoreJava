d6880300da566c4db8fae4e9631008422f30bf4e
mohanakrishnajanmanchi <mohanakrishna.janmanchi@gmail.com>
2023-06-15 12:42:56 -0500

    committing June 12,13 assignment

--- src/June12/StringToIntegerConverter.java
+++ src/June12/StringToIntegerConverter.java
@@ -8,22 +8,20 @@ public class StringToIntegerConverter {
         String s4 = "123f";
         try {
             System.out.println("Input Number: " + s1 + " Converted number: " + convertStringToInteger(s1));
-        } catch (CustomExceptionClass e) {
-            System.out.println(e);
-        }
-        try {
             System.out.println("Input Number: " + s2 + " Converted number: " + convertStringToInteger(s2));
-        } catch (CustomExceptionClass e) {
-            System.out.println(e);
-        }
-        try {
             System.out.println("Input Number: " + s3 + " Converted number: " + convertStringToInteger(s3));
-        } catch (CustomExceptionClass e) {
-            System.out.println(e);
-        }
-        try {
             System.out.println("Input Number: " + s4 + " Converted number: " + convertStringToInteger(s4));
         } catch (CustomExceptionClass e) {
+            System.out.println(e);
+        }
+        catch (CustomExceptionClass e) {
+                System.out.println(e);
+            }
+        }
+        catch (CustomExceptionClass e) {
+            System.out.println(e);
+        }
+        catch (CustomExceptionClass e) {
             System.out.println(e);
         }
     }
